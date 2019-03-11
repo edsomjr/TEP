ef61e177ccd0ffa2669f52ff6863f06948fdd779
Edson Alves <edsomjr@gmail.com>
2019-03-11 20:04:48 -0300

    Continuação da segunda aula sobre árvores.

--- Estruturas_de_Dados/slides/TR-2/insert.cpp
+++ Estruturas_de_Dados/slides/TR-2/insert.cpp
@@ -1,16 +1,5 @@
-#include <iostream>
-#include <functional>
-
 template<typename T>
 class BST {
-    friend std::ostream& operator<<(std::ostream& os, const BST& tree)
-    {
-        tree.preorder(tree.root, 
-            [&](T value) { os << "[" << value << "] "; });
-
-        return os;
-    }
-
 private:
     struct Node {
         T info;
@@ -18,37 +7,6 @@ private:
     };
 
     Node *root;
-
-    void inorder(const Node* node, const std::function<void(T)>& f) const
-    {
-        if (node)
-        {
-            inorder(node->left, f);
-            f(node->info);
-            inorder(node->right, f);
-        }
-    }
-
-    void preorder(const Node* node, const std::function<void(T)>& f) const
-    {
-        if (node)
-        {
-            f(node->info);
-            inorder(node->left, f);
-            inorder(node->right, f);
-        }
-    }
-
-    void postorder(const Node* node, const std::function<void(T)>& f) const
-    {
-        if (node)
-        {
-            inorder(node->left, f);
-            inorder(node->right, f);
-            f(node->info);
-        }
-    }
-
 
 public:
     BST() : root(nullptr) {}
@@ -79,16 +37,3 @@ public:
             prev->right = node;
     }
 };
-
-int main()
-{
-    BST<int> tree;
-
-    tree.insert(2);
-    tree.insert(1);
-    tree.insert(3);
-
-    std::cout << tree << '\n';
-
-    return 0;
-}
