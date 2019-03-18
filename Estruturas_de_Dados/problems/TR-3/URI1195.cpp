e1c6e9046d16b6a5ec0fba42b9f405e245103ea2
Edson Alves <edsomjr@gmail.com>
2019-03-18 12:14:42 -0300

    Exercício sobre árvores binárias.

---
+++ Estruturas_de_Dados/problems/TR-3/URI1195.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+struct BST {
+    struct Node
+    {
+        int info;
+        Node *left, *right;
+    };
+
+    Node *root;
+
+    BST() : root(nullptr) {}
+
+    void inorder(const Node* node) const
+    {
+        if (node) {
+            inorder(node->left);
+            std::cout << ' ' << node->info;
+            inorder(node->right);
+        }
+    }
+
+    void preorder(const Node* node) const
+    {
+        if (node)
+        {
+            std::cout << ' ' << node->info;
+            preorder(node->left);
+            preorder(node->right);
+        }
+    }
+
+    void postorder(const Node* node) const
+    {
+        if (node)
+        {
+            postorder(node->left);
+            postorder(node->right);
+            std::cout << ' ' << node->info;
+        }
+    }
+
+    void insert(int info)
+    {
+        Node **node = &root;
+        
+        while (*node)
+        {
+            if ((*node)->info == info)
+                return;
+            else if (info < (*node)->info)
+                node = &(*node)->left;
+            else
+                node = &(*node)->right;
+        }
+        
+        *node = new Node { info, nullptr, nullptr };
+    }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+
+    int C;
+    std::cin >> C;
+
+    for (int test = 1; test <= C; ++test)
+    {
+        int N;
+        std::cin >> N;
+
+        BST tree;
+
+        while (N--)
+        {
+            int info;
+            std::cin >> info;
+
+            tree.insert(info);
+        }
+
+        std::cout << "Case " << test << ":\n";
+
+        std::cout << "Pre.:";
+        tree.preorder(tree.root);
+        std::cout << "\n";
+
+        std::cout << "In..:";
+        tree.inorder(tree.root);
+        std::cout << "\n";
+
+        std::cout << "Post:";
+        tree.postorder(tree.root);
+        std::cout << "\n\n";
+   }
+
+   return 0;
+}
---
+++ Estruturas_de_Dados/problems/TR-3/URI1466.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct BST {
+    struct Node
+    {
+        int info;
+        Node *left, *right;
+    };
+
+    Node *root;
+
+    BST() : root(nullptr) {}
+
+    void BFS() const 
+    {
+        vector<int> xs;
+        queue<Node *> q;
+        q.push(root);
+
+        while (not q.empty()) {
+            auto node = q.front();
+            q.pop();
+
+            if (node) {
+                xs.push_back(node->info);
+                q.push(node->left); 
+                q.push(node->right); 
+            }
+        }
+
+        for (size_t i = 0; i < xs.size(); ++i)
+            cout << xs[i] << (i + 1 == xs.size() ? '\n' : ' ');
+        cout << '\n';
+    }
+
+    void insert(int info)
+    {
+        Node **node = &root;
+        
+        while (*node)
+        {
+            if ((*node)->info == info)
+                return;
+            else if (info < (*node)->info)
+                node = &(*node)->left;
+            else
+                node = &(*node)->right;
+        }
+        
+        *node = new Node { info, nullptr, nullptr };
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    int C;
+    cin >> C;
+
+    for (int test = 1; test <= C; ++test)
+    {
+        int N;
+        cin >> N;
+
+        BST tree;
+
+        while (N--)
+        {
+            int info;
+            cin >> info;
+
+            tree.insert(info);
+        }
+
+        cout << "Case " << test << ":\n";
+        tree.BFS();
+   }
+
+   return 0;
+}
