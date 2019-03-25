f64f9e19f060479ca0cd9f9e6e55ad5be901bd07
Edson Alves <edsomjr@gmail.com>
2019-03-25 15:08:56 -0300

    Exercícios sobre pilhas.

---
+++ Estruturas_de_Dados/problems/PF-1/612C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<char, char> open { {'>', '<'}, {']', '['}, {'}', '{'}, {')', '('} };
+
+int solve(const string& S)
+{
+    stack<char> st;
+    int ans = 0;
+
+    for (const auto& c : S)
+    {
+        switch (c) {
+        case '(':
+        case '<':
+        case '{':
+        case '[':
+            st.push(c);
+            break;
+
+        default:
+            if (st.empty())
+                return -1;
+
+            ans += (open[c] == st.top() ? 0 : 1);
+            st.pop();
+        }
+    }
+
+    return st.empty() ? ans : -1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    string S;
+    cin >> S;
+
+    auto ans = solve(S);
+
+    if (ans == -1)
+        cout << "Impossible\n";
+    else
+        cout << ans << '\n';
+
+    return 0;
+}
