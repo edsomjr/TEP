be555e4413c853f96fe5b76d33e90583fee38df4
Edson Alves <edsomjr@gmail.com>
2019-04-15 17:32:49 -0300

    Início da terceira aula de strings.

---
+++ Strings/slides/ST-3/memory.cpp
@@ -0,0 +1,9 @@
+// Esta implementação assume que o alfabeto é composto
+// pelas 26 letras maiúsculas do alfabeto
+void histogram(int h[26], const string& s)
+{
+    memset(h, 0, sizeof h);
+
+    for (auto c : s)
+        ++h[c - 'A'];
+}
