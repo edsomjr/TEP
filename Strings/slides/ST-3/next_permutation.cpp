fc503f82abc8bf719e538be626ff8e9cd0e0c1cd
Edson Alves <edsomjr@gmail.com>
2019-04-16 11:47:21 -0300

    Finalização da terceira aula de strings.

---
+++ Strings/slides/ST-3/next_permutation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s { "banana" };
+    int number = 0;
+
+    sort(s.begin(), s.end());
+
+    do {
+        printf("%02d. %s\n", ++number, s.c_str());
+    } while (next_permutation(s.begin(), s.end()));
+ 
+    printf("%s has %d anagrams\n", s.c_str(), number);
+
+    return 0;
+}
