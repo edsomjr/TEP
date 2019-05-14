2166c046c536941db7ec3ca5394cee22ed05e6ef
Durval Carvalho <durvalcsouza@outlook.com>
2019-05-14 20:08:56 -0300

    modificação na função compression

--- Estruturas_de_Dados/slides/FT-2/inversions.cpp
+++ Estruturas_de_Dados/slides/FT-2/inversions.cpp
@@ -1,14 +1,25 @@
 template<typename T>
-long long count_inversions(const vector<T>& as)
+long long count_inversions_compression(const vector<T>& as)
 {
-    T _max = *max_element(as.begin(), as.end());
-    BITree<T> ft(_max);
-    
+    vector<T> xs(as);
+
+    sort(xs.begin(), xs.end());
+
+    map<T, int> f;
+
+    for (size_t i = 1; i <= as.size(); ++i)
+        f[ xs[i-1] ] = i;
+
+    BITree<T> ft(as.size());
+
     long long inversions = 0;
-    for (size_t i = 0; i < as.size(); ++i)
+
+    for(size_t i=0; i<as.size(); i++)
     {
-        inversions += ft.RSQ(as[i], _max);
-        ft.add(as[i], 1);
+        auto comp_val = f[as[i]];
+
+        inversions += ft.RSQ(comp_val, as.size());
+        ft.add(comp_val, 1);
     }
 
     return inversions;
