8abbc10a674c50c4a291f4064a26fe6bbe458c09
Edson Alves <edsomjr@gmail.com>
2019-06-24 11:43:38 -0300

    Finalização dos exercícios sobre Bellman-Ford.

--- Grafos/problems/SSP-1/1205.cpp
+++ Grafos/problems/SSP-1/1205.cpp
@@ -2,21 +2,38 @@
 
 using namespace std;
 
+struct Edge { int u, v; };
+
 const int MAX { 1010 }, oo { 1000000010 };
-
 int dist[MAX], soldiers[MAX];
-vector<int> adj[MAX];
 
-void solve(int s, int e, int N, const vector<edge>& edges)
+double
+solve(int s, int e, int N, int K, double P, const vector<Edge>& edges)
 {
     for (int i = 1; i <= N; ++i)
         dist[i] = oo;
 
-    dist[s] = 0;
+    dist[s] = soldiers[s];
 
-    for (int i = 1; i <= N - 1; i++)
-        for (const auto& [u, v, w] : edges)
-            dist[v] = min(dist[v], dist[u] + w);
+    for (int i = 1; i <= N; i++)
+    {
+        bool updated = false;
+
+        for (const auto& edge : edges)
+            if (dist[edge.v] > dist[edge.u] + soldiers[edge.v])
+            {
+                dist[edge.v] = dist[edge.u] + soldiers[edge.v];
+                updated = true;
+            }
+
+        if (not updated)
+            break;
+    }
+
+    if (dist[e] > K)
+        return 0.0;
+
+    return pow(P, dist[e]);
 }
 
 int main()
@@ -28,6 +45,37 @@ int main()
 
     while (cin >> N >> M >> K >> P)
     {
+        memset(soldiers, 0, sizeof soldiers);
+
+        vector<Edge> edges;
+
+        while (M--)
+        {
+            int u, v;
+            cin >> u >> v;
+
+            edges.push_back(Edge { u, v });
+            edges.push_back(Edge { v, u });
+        }
+
+        int A;
+        cin >> A;
+
+        while (A--)
+        {
+            int pos;
+            cin >> pos;
+
+            soldiers[pos]++;
+        }
+
+        int s, e;
+        cin >> s >> e;
+
+        auto ans = solve(s, e, N, K, P, edges);
+
+        cout.precision(3);
+        cout << fixed << ans << '\n';
     }
 
     return 0;
