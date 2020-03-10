2867191f1033d42601269d5387919ee1624fee97
Edson Alves <edsomjr@gmail.com>
2020-03-10 18:32:44 -0300

    Finalização dos exercícios sobre árvores de segmentos.

--- Estruturas_de_Dados/problems/segtree/codes/339D.cpp
+++ Estruturas_de_Dados/problems/segtree/codes/339D.cpp
@@ -1,93 +1,69 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+using Op = int (*)(int, int);
 
-#define MAX 1000010
-
-int tree[MAX];
-
-int lor(int a, int b)
+class SegmentTree
 {
-    return a | b;
-}
+    int N;
+    std::vector<int> ns;
+    
+public:
+    SegmentTree(const std::vector<int>& xs) : N(xs.size()), ns(2*N)
+    {
+        std::copy(xs.begin(), xs.end(), ns.begin() + N);
 
-int lxor(int a, int b)
-{
-    return a ^ b;
-}
+        int k = N;
+
+        Op op = [](int x, int y) { return x | y; };
+        Op next = [](int x, int y) { return x ^ y; };
+
+        while (k >>= 1)
+        {
+            for (int i = k; i < 2*k; ++i)
+                ns[i] = op(ns[2*i], ns[2*i + 1]);
+
+            swap(op, next);
+        }
+    }
+
+    int update(int i, int value)
+    {
+        int a = i + N - 1;
+        ns[a] = value;
+
+        Op op = [](int x, int y) { return x | y; };
+        Op next = [](int x, int y) { return x ^ y; };
+
+        while (a >>= 1) {
+            ns[a] = op(ns[2*a], ns[2*a + 1]);
+            swap(op, next);
+        }
+
+        return ns[1];
+    }
+};
 
 int main()
 {
+    ios::sync_with_stdio(false);
+
     int n, m;
-    scanf("%d %d", &n, &m);
+    cin >> n >> m;
 
-    int qtd = 1 << n;
-    int pos = qtd;
+    vector<int> xs(1 << n);
 
-    for (int i = 0; i < qtd; ++i)
-        scanf("%d", tree + pos + i);
+    for (int i = 0; i < (1 << n); ++i)
+        cin >> xs[i];
 
- /*   for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", i);
-    printf("\n");
+    SegmentTree tree(xs);
 
-    for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", tree[i]);
-    printf("\n");
-*/
-    pos >>= 1;
-    int (*op)(int, int) = lor, (*next)(int, int) = lxor;
-
-    while (pos)
-    {
-        for (int i = pos; i < 2*pos; ++i)
-        {
-            tree[i] = op(tree[2*i], tree[2*i + 1]);
-        }
-        swap(op, next);
-        pos >>= 1;
-/*    for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", i);
-    printf("\n");
-
-    for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", tree[i]);
-    printf("\n"); */
-    }
- 
-//printf("queries:\n");
     while (m--)
     {
         int p, b;
-        scanf("%d %d", &p, &b);
-        pos = qtd + p - 1;
-//printf("p = %d, b = %d\n", p, b);
-        tree[pos] = b;
+        cin >> p >> b;
 
-        int sib = pos % 2 ? pos - 1 : pos + 1;
-        int par = pos / 2;
-
-        op = lor;
-        next = lxor;
-
-        while (par)
-        {
-            tree[par] = op(tree[pos], tree[sib]);
-//printf("par = %d, tree[%d] = %d, pos = %d, sib = %d\n", par, par, tree[par], pos, sib);
-            pos = par;
-            sib = par % 2 ? pos - 1 : pos + 1;
-            swap(op, next);
-            par >>= 1;
-        }
- /*    for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", i);
-    printf("\n");
-
-    for (int i = 1; i < 2*qtd; ++i) 
-        printf("%3d", tree[i]);
-    printf("\n"); */
-
-        printf("%d\n", tree[1]);
+        cout << tree.update(p, b) << '\n';
     }
 
     return 0;
