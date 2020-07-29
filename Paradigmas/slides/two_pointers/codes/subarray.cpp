a5cc319d7b14af51f6387ebb47728370a497e22e
Edson Alves <edsomjr@gmail.com>
2020-07-29 10:41:58 -0300

    Correção de bug na implementação.

--- Paradigmas/slides/two_pointers/codes/subarray.cpp
+++ Paradigmas/slides/two_pointers/codes/subarray.cpp
@@ -8,22 +8,14 @@ size_t max_subarray(const vector<int>& xs, size_t K)
 
     while (L < N)
     {
-        odds += (xs[L] % 2);
+        R = max(L, R);
 
-        if (odds <= K)
-        {
-            R = max(L + 1, R);
+        while (R < N and (xs[R] % 2 == 0 or odds < K))
+            odds += (xs[R++] % 2);
 
-            while (R < N and (xs[R] % 2 == 0 or odds < K))
-            {
-                odds += (xs[R] % 2);
-                ++R;
-            }
+        ans = max(ans, R - L);
 
-            ans = max(ans, R - L);
-        }
- 
-        odds -= (xs[L] % 2);
+        odds = max(odds - (xs[L] % 2), 0ul);
         ++L;
     }
 
@@ -32,10 +24,11 @@ size_t max_subarray(const vector<int>& xs, size_t K)
 
 int main()
 {
-    vector<int> xs { 1, 2, 3, 4, 5 }, ys { 1, 3, 5 };
+    vector<int> xs { 1, 3, 5, 4, 5 }, ys { 1, 3, 5 };
 
     cout << max_subarray(xs, 1) << '\n';
     cout << max_subarray(xs, 2) << '\n';
+    cout << max_subarray(xs, 3) << '\n';
     cout << max_subarray(ys, 0) << '\n';
 
     return 0;
