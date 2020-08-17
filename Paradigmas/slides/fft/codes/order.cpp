dc849084edddf73c0afe0beed23f44ba212bee26
Edson Alves <edsomjr@gmail.com>
2020-08-17 20:05:50 -0300

    Início do texto sobre a FFT.

---
+++ Paradigmas/slides/fft/codes/order.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int reversed(int x, int bits)
+{
+    int res = 0;
+
+    for (int i = 0; i < bits; ++i)
+    {
+        res <<= 1;
+        res |= (x & 1);
+        x >>= 1;
+    }
+
+    return res;
+}
+
+template<typename T> vector<T> sortByBits(const vector<T>& xs)
+{
+    int N = (int) xs.size(), bits = 1;
+
+    while ((1 << bits) != N)
+        ++bits;
+
+    vector<int> is(N);
+    iota(is.begin(), is.end(), 0);
+
+    sort(is.begin(), is.end(), [&bits](int x, int y) {
+        return reversed(x, bits) < reversed(y, bits);
+    });
+
+    vector<T> ans(N);
+
+    for (int i = 0; i < N; ++i)
+        ans[i] = xs[is[i]];
+
+    return ans;
+} 
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> xs(N);
+    iota(xs.begin(), xs.end(), 0);
+
+    auto ys = sortByBits(xs);
+
+    for (int i = 0; i < N; ++i)
+        cout << ys[i] << (i + 1 == N ? '\n' : ' ');
+
+    return 0;
+}
