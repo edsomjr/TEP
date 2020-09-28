8c5d7cb4971b6fd26340d80bcf5c57efe792d18f
Edson Alves <edsomjr@gmail.com>
2020-09-28 17:30:24 -0300

    Remoção de bug no código.

--- Paradigmas/slides/max_range_sum/codes/max-2D-range-sum_kadane.cpp
+++ Paradigmas/slides/max_range_sum/codes/max-2D-range-sum_kadane.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 const int oo { 1000000010 };
 
-int kadane(const vector<int>& as)
+int kadane(int N, const vector<int>& as)
 {
-    vector<int> s(as.size());
-    s[0] = as[0];
+    vector<int> s(N + 1);
+    s[1] = as[1];
 
-    for (size_t i = 1; i < as.size(); ++i)
+    for (size_t i = 2; i < as.size(); ++i)
         s[i] = max(as[i], s[i - 1] + as[i]);
 
-    return *max_element(s.begin(), s.end());
+    return *max_element(s.begin() + 1, s.end());
 }
 
 int MSR(int N, int M, const vector<vector<int>>& A)
@@ -20,7 +20,7 @@ int MSR(int N, int M, const vector<vector<int>>& A)
     vector<vector<int>> p(N + 1, vector<int>(M + 1, 0));
     int ans = -oo;
 
-    for (int i = 1; i <= N; ++i)
+    for (int i = 1; i <= M; ++i)
     {
         vector<int> r(N + 1, 0);
 
@@ -29,7 +29,7 @@ int MSR(int N, int M, const vector<vector<int>>& A)
             for (int k = 1; k <= N; ++k)
                 r[k] += A[k][j]; 
 
-            ans = max(ans, kadane(r));
+            ans = max(ans, kadane(N, r));
         }
     }
 
