a01c4d67b94bac3cb6296fb4ddb9facea109e96d
Edson Alves <edsomjr@gmail.com>
2020-10-01 20:37:04 -0300

    Solução TLE para o problema "Cortando o papel".

--- Upsolving/OBI/2017_F2_NS/codes/papel.cpp
+++ Upsolving/OBI/2017_F2_NS/codes/papel.cpp
@@ -1,73 +1,92 @@
-// OBI 2017 - Nìvel Universitário: Fase 2
-// Cortando o Papel (https://olimpiada.ic.unicamp.br/pratique/pu/2017/f2/papel/) 
-// Referência: https://noic.com.br/materiais-informatica/comentario-old/obi-2017/9695-2/ 
 #include <bits/stdc++.h>
 
+#ifdef DEBUG
+#define LOG(var, sep) (cerr << #var << " = " << (var) << (sep))
+
+#define LOGM(msg) (cerr << (msg))
+
+#define LOGV(vec, sep) {                                        \
+    cerr << #vec << " =";                                       \
+    for (const auto& v : (vec)) cerr << " " << v;               \
+    cerr << (sep); }
+
+#define LOGD(dict, sep) {                                       \
+    cerr << #dict << " =\n";                                    \
+    for (const auto& [k, v] : (dict))                           \
+        cerr << k << " -> " << v << '\n';                       \
+    cerr << (sep); }
+
+#define LOGA(arr, N, sep) {                                     \
+        cerr << #arr << " =";                                   \
+        for (int i = 0; i < N; ++i) cerr << " " << (arr)[i];    \
+        cerr << (sep); }
+
+#define LOGA2(arr, N, M, sep) {                                 \
+        cerr << #arr << " =\n";                                 \
+        for (int i = 0; i < N; ++i) {                           \
+            for (int j = 0; j < M; ++j)                         \
+                cerr << (arr)[i][j] << " ";                     \
+            cerr << endl;                                       \
+        } cerr << (sep); }
+#else
+#define LOG(var, sep)
+#define LOGM(msg)
+#define LOGV(vec, sep)
+#define LOGA(arr, N, sep)
+#define LOGA2(arr, N, M, sep)
+#endif
+
 using namespace std;
+using ll = long long;
 using ii = pair<int, int>;
 
-int pieces(double x, int N, const vector<int>& hs)
+using namespace std;
+
+int solve(int N, vector<int>& hs)
 {
-    auto L = 0, res = 0;
+    LOGV(hs, endl);
+    LOG(N, endl);
+    // Compressão dos adjacentes iguais
+    auto it = unique(hs.begin(), hs.end());
 
-    while (L < N)
-    {
-        if (hs[L] <= x)
-        {
-            ++L;
-            continue;
-        }
+    N = (int) distance(hs.begin(), it);
+    hs.resize(N);
 
-        auto R = L + 1;
-
-        while (R < N and hs[R] > x)
-            ++R;
-
-        ++res;
-        L = R;
-    }
-
-    return res + 1;
-}
-
-int solve(int N, const vector<int>& hs)
-{
-    // Compressão dos adjacentes iguais e adição de bordas
-    vector<int> xs(1, 0);
+    // Identificação dos vales e picos
+    vector<int> cs;
  
-    for (auto h : hs)
-        if (xs.back() != h)
-            xs.push_back(h);
+    for (int i = 1; i <= N - 2; ++i)
+        if ((hs[i - 1] < hs[i] and hs[i] > hs[i + 1]) ||
+            (hs[i - 1] > hs[i] and hs[i] < hs[i + 1]))
+                cs.push_back(hs[i]);
 
-    xs.push_back(0);
-    
-    N = (int) xs.size();
+    // Finaliza com um vale
+    if (cs.size() % 2) 
+        cs.push_back(0);
 
-    // Identificação dos valores e picos
-    map<int, int> ys;
-    const int PICO = -1, VALE = 1;
- 
-    for (int i = 1; i < N - 1; ++i)
+    // Geração dos eventos para o sweep line
+    vector<ii> es;
+    int OPEN = 1, CLOSE = -1;
+
+    for (size_t i = 0; i < cs.size(); i += 2)
     {
-        if (xs[i - 1] < xs[i] and xs[i] > xs[i + 1])
-            ys[xs[i]] += PICO;
-
-        if (xs[i - 1] > xs[i] and xs[i] < xs[i + 1])
-            ys[xs[i]] += VALE;
+        es.push_back(ii(cs[i + 1], OPEN));
+        es.push_back(ii(cs[i], CLOSE));
     }
 
-    // Processamento dos picos e valores
-    int ans = 2, pieces = 2;
+    sort(es.begin(), es.end());
 
-    for (auto y : ys)
+    int ans = 1, open = 0;
+
+    for (auto e : es)
     {
-        auto delta = y.second;
+        auto t = e.second;
 
-        pieces += delta;
-        ans = max(ans, pieces);
+        open += t;
+        ans = max(ans, open);
     }
 
-    return ans;
+    return ans + 1;
 }
 
 int main()
@@ -77,9 +96,9 @@ int main()
     int N;
     cin >> N;
 
-    vector<int> hs(N);
+    vector<int> hs(N + 2, 0);
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 1; i <= N; ++i)
         cin >> hs[i];
 
     auto ans = solve(N, hs);
