5692f3f858d46ae05d3e04bab613e1e9fcdee6ca
Edson Alves <edsomjr@gmail.com>
2020-10-02 19:47:02 -0300

    Ajuste da solução para melhor exibição nos slides.

--- Upsolving/OBI/2017_F2_NS/codes/papel.cpp
+++ Upsolving/OBI/2017_F2_NS/codes/papel.cpp
@@ -1,58 +1,17 @@
 #include <bits/stdc++.h>
 
-#ifdef DEBUG
-#define LOG(var, sep) (cerr << #var << " = " << (var) << (sep))
-
-#define LOGM(msg) (cerr << (msg))
-
-#define LOGV(vec, sep) {                                        \
-    cerr << #vec << " =";                                       \
-    for (const auto& v : (vec)) cerr << " " << v;               \
-    cerr << (sep); }
-
-#define LOGD(dict, sep) {                                       \
-    cerr << #dict << " =\n";                                    \
-    for (const auto& [k, v] : (dict))                           \
-        cerr << k << " -> " << v << '\n';                       \
-    cerr << (sep); }
-
-#define LOGA(arr, N, sep) {                                     \
-        cerr << #arr << " =";                                   \
-        for (int i = 0; i < N; ++i) cerr << " " << (arr)[i];    \
-        cerr << (sep); }
-
-#define LOGA2(arr, N, M, sep) {                                 \
-        cerr << #arr << " =\n";                                 \
-        for (int i = 0; i < N; ++i) {                           \
-            for (int j = 0; j < M; ++j)                         \
-                cerr << (arr)[i][j] << " ";                     \
-            cerr << endl;                                       \
-        } cerr << (sep); }
-#else
-#define LOG(var, sep)
-#define LOGM(msg)
-#define LOGV(vec, sep)
-#define LOGA(arr, N, sep)
-#define LOGA2(arr, N, M, sep)
-#endif
-
 using namespace std;
-using ll = long long;
 using ii = pair<int, int>;
-
-using namespace std;
 
 int solve(int N, vector<int>& hs)
 {
-    LOGV(hs, endl);
-    LOG(N, endl);
     // Compressão dos adjacentes iguais
     auto it = unique(hs.begin(), hs.end());
 
     N = (int) distance(hs.begin(), it);
     hs.resize(N);
 
-    // Identificação dos vales e picos
+    // Identificação dos pontos críticos
     vector<int> cs;
  
     for (int i = 1; i <= N - 2; ++i)
@@ -60,13 +19,13 @@ int solve(int N, vector<int>& hs)
             (hs[i - 1] > hs[i] and hs[i] < hs[i + 1]))
                 cs.push_back(hs[i]);
 
-    // Finaliza com um vale
+    // Finaliza com um mínimo
     if (cs.size() % 2) 
         cs.push_back(0);
 
     // Geração dos eventos para o sweep line
     vector<ii> es;
-    int OPEN = 1, CLOSE = -1;
+    int OPEN = 1, CLOSE = -1, ans = 1, open = 0;
 
     for (size_t i = 0; i < cs.size(); i += 2)
     {
@@ -76,13 +35,9 @@ int solve(int N, vector<int>& hs)
 
     sort(es.begin(), es.end());
 
-    int ans = 1, open = 0;
-
     for (auto e : es)
     {
-        auto t = e.second;
-
-        open += t;
+        open += e.second;
         ans = max(ans, open);
     }
 
@@ -101,9 +56,7 @@ int main()
     for (int i = 1; i <= N; ++i)
         cin >> hs[i];
 
-    auto ans = solve(N, hs);
-
-    cout << ans << '\n';
+    cout << solve(N, hs) << '\n';
 
     return 0;
 }
