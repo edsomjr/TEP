9d5ed84e3ed0023cf6ec165b9d41ecc246f1a9cc
Edson Alves <edsomjr@gmail.com>
2020-10-07 19:29:07 -0300

    Finalização do upsolving do ABC 043.

--- Upsolving/AtCoder/B043/codes/D.cpp
+++ Upsolving/AtCoder/B043/codes/D.cpp
@@ -1,72 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
 
-ll fast_exp(ll a, ll n, ll m)
+pair<int, int> solve(const string& s)
 {
-    ll res = 1, base = a;
+    int N = (int) s.size();
 
-    while (n)
-    {
-        if (n & 1)
-            res = (res * base) % m;
+    // Caso 1: dois caracteres consecutivos
+    for (int i = 1; i < N; ++i)
+        if (s[i] == s[i - 1])
+            return { i, i + 1 };
 
-        base = (base * base) % m;
-        n >>= 1;
-    }
+    // Caso 2: três caracteres consecutivos, extremos iguais 
+    for (int i = 2; i < N; ++i)
+        if (s[i] == s[i - 2])
+            return { i - 1, i + 1 };
 
-    return res;
-}
-
-const ll MOD { 1000000007 };
-const int MAX { 200010 };
-
-ll fact[MAX], inv[MAX];
-
-ll binom(int n, int m)
-{
-    ll res = (fact[n] * inv[m]) % MOD;
-    return (res * inv[n - m]) % MOD;
-}
-
-void precomp()
-{
-    fact[0] = inv[0] = 1;
-
-    for (int n = 1; n < MAX; ++n)
-        fact[n] = (n * fact[n - 1]) % MOD;
-
-    for (int n = 1; n < MAX; ++n)
-        inv[n] = fast_exp(fact[n], MOD - 2, MOD);
-}
-
-ll solve(int H, int W, int A, int B)
-{
-    ll ans = 0;
-
-    for (int x = B + 1; x <= W; ++x)
-    {
-        auto m1 = x - 1, n1 = H - A - 1;
-        auto m2 = W - x + 1 - 1, n2 = A - 1;
-        auto ways = (binom(m1 + n1, m1) * binom(m2 + n2, m2)) % MOD;
-
-        ans = (ans + ways) % MOD;
-    }
-
-    return ans;
+    return { -1, -1 };
 }
 
 int main()
 {
-    precomp();
+    ios::sync_with_stdio(false);
 
-    int H, W, A, B;
-    cin >> H >> W >> A >> B;
+    string s;
+    cin >> s;
 
-    auto ans = solve(H, W, A, B);
+    auto ans = solve(s);
 
-    cout << ans << '\n';
+    cout << ans.first << ' ' << ans.second << '\n';
 
     return 0;
 }
