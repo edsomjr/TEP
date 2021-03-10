4a08791223d4732cbd6c18eddc2d71cc2788008b
Edson Alves <edsomjr@gmail.com>
2021-03-10 18:06:50 -0300

    Adição das soluções dos exercícios propostos.

---
+++ Matematica/slides/numeros_primos/codes/11752.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> composite(int m)
+{
+    vector<int> cs;
+
+    for (int n = 2; n < m; ++n)
+        for (int d = 2; d * d <= n; ++d)
+            if (n % d == 0)
+            {
+                cs.push_back(n);
+                break;
+            }
+
+    return cs;
+}
+
+unsigned long long power(int a, int n)
+{
+    unsigned long long res = 1;
+
+    while (n--)
+        res *= a;
+
+    return res;
+}
+
+set<unsigned long long> solve()
+{
+    auto cs = composite(64);
+    set<unsigned long long> ans;
+ 
+    for (int n = 1; n < (1 << 16); ++n)
+    {
+        for (auto c : cs)
+
+        if (c*log2(n) < 64)
+            ans.insert(power(n, c));
+    }
+
+    return ans;
+}
+
+int main()
+{
+    auto ans = solve();
+
+    for (auto n : ans)
+        cout << n << '\n';
+
+    return 0;
+}
