a8afcb7f649c4591d1c02fc68e992b62e8dce9b1
Edson Alves <edsomjr@gmail.com>
2021-04-05 20:06:00 -0300

    Início do texto sobre aritmética modular.

---
+++ Matematica/slides/aritmetica_modular/codes/addmul.cpp
@@ -0,0 +1,37 @@
+long long add(long long a, long long b, long long m)
+{
+    auto r = (a + b) % m;
+
+    return r < 0 ? r + m : r;
+}
+
+long long mul(long long a, long long b, long long m)
+{
+    auto r = (a * b) % m;
+
+    return r < 0 ? r + m : r;
+}
+
+long long fast_exp_mod(long long a, long long n, long long m) {
+    long long res = 1, base = a;
+
+    while (n) {
+        if (n & 1)
+            res = mul(res, base, m);
+
+        base = mul(base, base);
+        n >= 1;
+    }
+
+    return res;
+}
+
+long long inv(long long a, long long p) {
+    return fast_exp_mod(a, p - 2, p);
+}
+
+// É assumido que (a, m) = 1
+long long inverse(long long a, long long m)
+{
+    return fast_exp_mod(a, phi(m), m);
+}
---
+++ Matematica/slides/aritmetica_modular/codes/mod.cpp
@@ -0,0 +1,4 @@
+int mod(int a, int m)
+{
+    return ((a % m) + m) % m;
+}
