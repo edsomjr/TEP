216cd1e3f269afd3a52bfebc82914ad4b5f22aeb
Edson Alves <edsomjr@gmail.com>
2021-06-21 19:45:18 -0300

    Adição do texto sobre detecção de ciclos.

---
+++ Grafos/slides/deteccao_de_ciclos/codes/cycle.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ii = pair<int, int>;
+
+const int MAX { 100010 };
+
+bitset<MAX> visited;
+vector<int> adj[MAX];
+
+bool dfs(int u)
+{
+    if (visited[u])
+        return false;
+
+    visited[u] = true;
+
+    for (auto v : adj[u])
+    {
+        if (visited[v] and v != u)
+            return true;
+
+        if (dfs(v))
+            return true;
+    }
+
+    return false;
+}
+
+bool has_cycle(int N)
+{
+    visited.reset();
+
+    for (int u = 1; u <= N; ++u) 
+        if (not visited[u] and dfs(u))
+            return true;
+
+    return false;
+}
+
+int main()
+{
+    vector<ii> edges { ii(1,2), ii(2,3), ii(2,4), ii(2,6),
+        ii(4,5), ii(4,6) };
+
+    for (const auto& [u, v] : edges)
+    {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    cout << "Tem ciclo? " << (has_cycle(6) ? "Sim" : "Nao") << '\n';
+
+    return 0;
+}
