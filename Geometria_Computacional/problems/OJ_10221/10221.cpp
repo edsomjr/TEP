de063bbda44b23707cf91bb386c8943488447c09
Edson Alves <edsomjr@gmail.com>
2022-02-16 10:04:19 -0300

    Atualização do PDF.

---
+++ Geometria_Computacional/problems/OJ_10221/10221.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+const int R { 6440 };
+const double PI = acos(-1.0);
+
+double arc(double s, double a)
+{
+    auto r = s + R;
+
+    return r*a;
+}
+
+double chord(double s, double a)
+{
+    auto r = s + R;
+
+    return 2*r*sin(a/2);
+}
+
+using namespace std;
+
+int main()
+{
+    double s, a;
+    string unit;
+
+    while (cin >> s >> a >> unit)
+    {
+        if (unit[0] == 'm') a /= 60;
+
+        if (a > 180) a = 360 - a;
+
+        a *= (PI/180.0);
+
+        printf("%.6f %.6f\n", arc(s, a), chord(s, a));
+    }
+
+    return 0;
+}
