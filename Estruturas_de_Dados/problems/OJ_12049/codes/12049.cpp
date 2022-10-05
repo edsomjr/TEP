9cf95e4f8fadfdaf5659abf2c0e64d83d951474b
Edson Alves <edsomjr@gmail.com>
2022-10-05 16:15:17 -0300

    Separação dos problemas.

---
+++ Estruturas_de_Dados/problems/OJ_12049/codes/12049.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(multiset<int>& s, const multiset<int>& r)
+{
+    vector<int> xs;
+    set_intersection(s.begin(), s.end(), r.begin(), r.end(), back_inserter(xs));
+
+    return s.size() + r.size() - 2*xs.size();
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M, x;
+        cin >> N >> M;
+
+        multiset<int> s, r;
+
+        while (N--)
+        {
+            cin >> x;
+            s.insert(x);
+        }
+
+        while (M--)
+        {
+            cin >> x;
+            r.insert(x);
+        }
+
+        cout << solve(s, r) << '\n';
+    }
+
+    return 0;
+}
