823a23a0f0390c8f356e67434d2f947d060d15cd
Edson Alves <edsomjr@gmail.com>
2023-03-13 11:51:59 -0300

    Adição do upsolving.

--- Upsolving/OJ/10303/codes/10303.py
+++ Upsolving/OJ/10303/codes/10303.py
@@ -5,8 +5,10 @@ import math
 def catalan(n):
     return str(math.factorial(2*n)//((n + 1)*math.factorial(n)**2))
 
+
 def solve(ns):
     return map(catalan, ns)
+
 
 if __name__ == '__main__':
     xs = sys.stdin.readlines()
