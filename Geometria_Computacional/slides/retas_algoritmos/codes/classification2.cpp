fefa57c7b0876ce2c164df3de25d03d243f9f943
kyomi <me@kyomi.dev>
2024-04-15 15:03:58 -0300

    Adicionar tipos genéricos ausentes em alguns métodos e funções

--- Geometria_Computacional/slides/retas_algoritmos/codes/classification2.cpp
+++ Geometria_Computacional/slides/retas_algoritmos/codes/classification2.cpp
@@ -2,7 +2,7 @@ template<typename T>
 struct Line {
     // Membros e construtores (equação geral}
 
-    bool operator==(const Line& r) const
+    bool operator==(const Line<T>& r) const
     {
         auto k = a ? a : b;
         auto s = r.a ? r.a : r.b;
@@ -10,7 +10,7 @@ struct Line {
         return equals(a*s, r.a*k) && equals(b*s, r.b*k) && equals(c*s, r.c*k);
     }
 
-    bool parallel(const Line& r) const
+    bool parallel(const Line<T>& r) const
     {
         auto det = a*r.b - b*r.a;
 
