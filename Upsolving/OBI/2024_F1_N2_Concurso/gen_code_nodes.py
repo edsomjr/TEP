2eed41fc343129d6a1405d4f4fd1d5943b4f0e0a
Edson Alves <edsomjr@gmail.com>
2024-07-13 08:41:05 -0300

    A geração dos nós inicia com todos os nós invisíveis.

--- Upsolving/OBI/2024_F1_N2_Concurso/gen_code_nodes.py
+++ Upsolving/OBI/2024_F1_N2_Concurso/gen_code_nodes.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 
 
@@ -22,14 +23,16 @@ if __name__ == '__main__':
     y = b
     count = 0
 
+    lines = []
+
     for i in range(N):
 
         if count == 22:
-            y = b
+            y = b - step
             x = 8
 
             print('vbar => Path')
-            print('@vbar.path = (7, 8) -- (7, 0) -- cycle')
+            print('@vbar.path = (7.5, 8) -- (7.5, 0) -- cycle')
             print('@vbar.color = gray')
             print('&vbar.dashed')
 
@@ -40,6 +43,11 @@ if __name__ == '__main__':
         print(f'\t@line{start}.anchor = west')
         print()
 
+        lines.append(f'-line{start}')
+
         y -= step
         start += 1
         count += 1
+
+    print('\n'.join(lines))
+
