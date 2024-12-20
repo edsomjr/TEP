49855cb6fb573c204feea93d3b8f96b26712685d
Isaac Menezes Pereira <isaacp.menezesp@gmail.com>
2024-12-19 21:31:10 -0300

    Update search.cpp
    Ajuste no limite do laço de busca das substrings P na string S. A ideia é que cada uma das n-m+1 substrings de tamanho m de S seja comparada com P.

--- Strings/slides/busca/codes/search.cpp
+++ Strings/slides/busca/codes/search.cpp
@@ -4,7 +4,7 @@ int occurrences(const string& P, const string& S)
     int n = S.size();
     int occ = 0;
 
-    for (int i = 0; i <= n - m; ++i) 
+    for (int i = 0; i <= n - m + 1; ++i) 
         occ += (P == S.substr(i, m) ? 1 : 0);
 
     return occ;
