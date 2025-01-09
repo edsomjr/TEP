3d03b42b501af9151db091cbd1cbddde9542a9de
Edson Alves <edsomjr@gmail.com>
2025-01-09 12:02:28 -0300

    Adição das pauses, revisão dos  códigos.

--- Strings/slides/cpp/codes/search2.cpp
+++ Strings/slides/cpp/codes/search2.cpp
@@ -14,47 +14,47 @@ double benchmark(const string& S, const string& P, const function<void(string,st
 
 int main()
 {
-    string S(1000000, 'a'), P { string(1000, 'a') + 'b' };
+    string S(1'000'000, 'a'), P { string(1'000, 'a') + 'b' };
 
     auto base = benchmark(S, P, [](auto s, auto p) 
         { for (size_t i = 0; i < s.size() + p.size(); ++i) {} });
 
     cout.precision(6);
-    cout << "Empty loop: \t\t" << base << " ms\t\t-\n";
+    cout << "Empty loop: \t\t" << base << " s\t\t-\n";
 
-    auto runtime = benchmark(S, P, [](auto s, auto p) { s.find(p); });
+    auto runtime = benchmark(S, P, [](auto s, auto p) { (void) s.find(p); });
 
-    cout << "find(): \t\t" << runtime << " ms\t\tx" << (int) round(runtime/base) << "\n";
+    cout << "find(): \t\t" << runtime << " s\t\tx" << (int) round(runtime/base) << "\n";
 
     runtime = benchmark(S, P, [](auto s, auto p) 
     {
         search(s.begin(), s.end(), p.begin(), p.end());
     });
 
-    cout << "search(): \t\t" << runtime << " ms\t\tx" << (int) round(runtime/base) << "\n";
+    cout << "search(): \t\t" << runtime << " s\t\tx" << (int) round(runtime/base) << "\n";
 
     runtime = benchmark(S, P, [](auto s, auto p) 
     {
         search(s.begin(), s.end(), boyer_moore_searcher(p.begin(), p.end()));
     });
 
-    cout << "boyer_moore: \t\t" << runtime << " ms\t\tx" << (int) round(runtime/base) << "\n";
+    cout << "boyer_moore: \t\t" << runtime << " s\t\tx" << (int) round(runtime/base) << "\n";
 
     runtime = benchmark(S, P, [](auto s, auto p) 
     {
         search(s.begin(), s.end(), boyer_moore_horspool_searcher(p.begin(), p.end()));
     });
 
-    cout << "boyer_moore_horspool: \t" << runtime << " ms\t\tx"
+    cout << "boyer_moore_horspool: \t" << runtime << " s\t\tx"
         << (int) round(runtime/base) << "\n";
 
     // Possível saída:
-    // Algoritmo                Runtime (em ms)       runtime/empty
-    // Empty loop:              0.000537077 ms        -
-    // find():                  0.027087 ms           x50
-    // search():                0.589431 ms           x1097
-    // boyer_moore:             0.00420558 ms         x8
-    // boyer_moore_horspool:    0.00327 ms            x6
+    // Algoritmo                Runtime (em s)      runtime/empty
+    // Empty loop:              0.00378854          -
+    // find():                  0.0930658           x25
+    // search():                4.07972             x1077
+    // boyer_moore:             0.0121259           x3
+    // boyer_moore_horspool:    0.00646972          x2
 
     return 0;
 }
