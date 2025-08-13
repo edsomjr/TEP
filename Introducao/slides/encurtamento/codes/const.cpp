14695169a61f1e62c23130664f699a00ff97d542
Edson Alves <edsomjr@gmail.com>
2025-08-13 13:19:34 -0300

    Ajustes no texto.

--- Introducao/slides/encurtamento/codes/const.cpp
+++ Introducao/slides/encurtamento/codes/const.cpp
@@ -1,4 +1,4 @@
-const int MAX { 1'000'010 };
-const ll oo { (1 << 62) };
-const ll MOD { 1'000'000'007 };
-const double PI = acos(-1.0);
+constexpr int MAX { 1'000'010 };
+constexpr ll oo { 1LL << 62 };
+constexpr ll MOD { 1'000'000'007 };
+constexpr double PI { acos(-1.0) };
