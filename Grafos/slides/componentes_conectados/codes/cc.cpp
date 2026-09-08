deae3cb44155d3c144dd528db4158babf8be177a
Edson Alves <edsomjr@gmail.com>
2026-09-08 15:38:07 -0300

    Atualização dos PDFs.

--- Grafos/slides/componentes_conectados/codes/cc.cpp
+++ Grafos/slides/componentes_conectados/codes/cc.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using ii = pair<int, int>;
 
-const int MAX { 100010 };
+const int MAX { 100'010 };
 bitset<MAX> visited;
 vector<int> adj[MAX];
 
@@ -44,10 +44,10 @@ int main()
     vector<ii> edges { ii(1, 2), ii(1, 8), ii(2, 10), ii(3, 7), ii(4, 5),
         ii(4, 9), ii(5, 9) };
 
-    for (const auto& [u, v] : edges)
+    for (auto [u, v] : edges)
     {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
     }
 
     connected_components(10);
